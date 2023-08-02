33a87d2c5d72d4b321e061d37201fa22321d799a
DarrenLuo <jn296@qq.com>
2023-08-02 22:02:07 +0800

    添加 jascypt 支持

--- nacos-config-spring-boot-autoconfigure/src/main/java/com/alibaba/boot/nacos/config/util/NacosConfigPropertiesUtils.java
+++ nacos-config-spring-boot-autoconfigure/src/main/java/com/alibaba/boot/nacos/config/util/NacosConfigPropertiesUtils.java
@@ -18,7 +18,19 @@ package com.alibaba.boot.nacos.config.util;
 
 import com.alibaba.boot.nacos.config.NacosConfigConstants;
 import com.alibaba.boot.nacos.config.properties.NacosConfigProperties;
-import com.alibaba.nacos.api.config.ConfigService;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyDetector;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyFilter;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertySourceConverter;
+import com.ulisesbocchio.jasyptspringboot.InterceptionMode;
+import com.ulisesbocchio.jasyptspringboot.configuration.EnvCopy;
+import com.ulisesbocchio.jasyptspringboot.detector.DefaultLazyPropertyDetector;
+import com.ulisesbocchio.jasyptspringboot.encryptor.DefaultLazyEncryptor;
+import com.ulisesbocchio.jasyptspringboot.environment.EncryptableEnvironment;
+import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment;
+import com.ulisesbocchio.jasyptspringboot.filter.DefaultLazyPropertyFilter;
+import com.ulisesbocchio.jasyptspringboot.resolver.DefaultLazyPropertyResolver;
+import org.jasypt.encryption.StringEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,9 +38,11 @@ import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 
-import java.util.Properties;
-import java.util.function.Function;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Springboot used to own property binding configured binding
@@ -44,7 +58,22 @@ public class NacosConfigPropertiesUtils {
 	public static NacosConfigProperties buildNacosConfigProperties(
 			ConfigurableEnvironment environment) {
 		NacosConfigProperties nacosConfigProperties = new NacosConfigProperties();
-		Binder binder = Binder.get(environment);
+		
+		logger.info("Initializing Environment: {}", environment.getClass().getSimpleName());
+		InterceptionMode actualInterceptionMode = InterceptionMode.WRAPPER;
+		List<Class<PropertySource<?>>> actualSkipPropertySourceClasses = Collections.emptyList();
+		EnvCopy envCopy = new EnvCopy(environment);
+		EncryptablePropertyFilter actualFilter = new DefaultLazyPropertyFilter(envCopy.get());
+		StringEncryptor actualEncryptor = new DefaultLazyEncryptor(envCopy.get());
+		EncryptablePropertyDetector actualDetector = new DefaultLazyPropertyDetector(envCopy.get());
+		EncryptablePropertyResolver actualResolver = new DefaultLazyPropertyResolver(actualDetector, actualEncryptor, environment);
+		EncryptablePropertySourceConverter converter = new EncryptablePropertySourceConverter(actualInterceptionMode, actualSkipPropertySourceClasses, actualResolver, actualFilter);
+		converter.convertPropertySources(environment.getPropertySources());
+		MutablePropertySources encryptableSources = converter.convertMutablePropertySources(InterceptionMode.WRAPPER, environment.getPropertySources(), envCopy);
+		EncryptableEnvironment standardEncryptableEnvironment = new StandardEncryptableEnvironment();
+		standardEncryptableEnvironment.setEncryptablePropertySources(encryptableSources);
+		
+		Binder binder = Binder.get(standardEncryptableEnvironment);
 		ResolvableType type = ResolvableType.forClass(NacosConfigProperties.class);
 		Bindable<?> target = Bindable.of(type).withExistingValue(nacosConfigProperties);
 		binder.bind(NacosConfigConstants.PREFIX, target);
